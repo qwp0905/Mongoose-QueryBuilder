81bfcbb702165e0cbfab1c41be3d574bb44390f6
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-08-25 20:35:27 +0900

    fix pull type

--- src/types/update.type.ts
+++ src/types/update.type.ts
@@ -35,7 +35,7 @@ export type SortOperator<T> = T extends NotObject
 export type PullOperator<T> = {
   [P in QueryKey<T>]?: QueryValue<T, P> extends Array<infer U>
     ? U extends NotObject
-      ? IQuerySelector<U>
+      ? Omit<IQuerySelector<U>, '$eq'>
       : IRootQuerySelector<U> & {
           [K in QueryKey<U>]?: ISelector<QueryValue<U, K>>
         }
